532f2019ef951c2a3a81a1b481afdb26d6b7ef97
Richard Lane <richard.lane@bristol.ac.uk>
2021-09-07 14:23:41 +0100

    init

---
+++ boosts.py
@@ -0,0 +1,143 @@
+"""
+Lorentz boosts
+
+"""
+import numpy as np
+
+
+def _to_inv_gev(time):
+    """
+    Convert from ps to GeV^-1
+
+    """
+    # Magic conversion factor from ps to inverse GeV is (3/1.97) * 10^12
+    return 3 * time * 10 ** 12 / 1.97
+
+
+def _to_ps(time):
+    return 1.97 * time / (3 * 10 ** 12)
+
+
+def _gamma(mass, energy):
+    """
+    Easiest way to calculate gamma, i reckon
+
+    Both in GeV
+
+    """
+    return energy / mass
+
+
+def _velocity(gamma):
+    return np.sqrt(1 - (1 / (gamma ** 2)))
+
+
+def _magnitude(vector: np.ndarray):
+    return np.sqrt(np.sum(vector ** 2))
+
+
+def direction(vector: np.ndarray):
+    """
+    Return a unit vector parallel to the input - three dimensional
+
+    """
+    return vector / _magnitude(vector)
+
+
+def _masses(energies, px, py, pz):
+    """
+    Relativistic mass
+
+    """
+    return np.sqrt((energies ** 2) - (px ** 2) - (py ** 2) - (pz ** 2))
+
+
+def _multiply(array, matrix):
+    """
+    multiply each row of a matrix by each element of an array
+
+    e.g. [1, 2] * [[1, 2, 3], [4, 5, 6]] = [[1, 2, 3], [8, 10, 12]]
+
+    """
+    return array.reshape((-1,) + (1,) * (matrix.ndim - 1)) * matrix
+
+
+def boost(target, gamma, direction: np.ndarray):
+    """
+    Boost a 4-vector (original) in a direction
+
+    4-vector in GeV: (x, y, z, t)
+
+    """
+    assert len(target) == 4
+
+    # Work out v from gamma
+    v = _velocity(gamma)
+
+    new_energy = gamma * (target[3] - v * np.dot(direction, target[:3]))
+    new_p = (
+        target[:3]
+        + (gamma - 1) * np.dot(direction, target[:3]) * direction
+        - gamma * target[3] * v * direction
+    )
+
+    return [*new_p, new_energy]
+
+
+def boosts(*particles, target: np.ndarray):
+    """
+    Boost some particles into the rest frame of targets
+
+    Each particle in particles should be an array of [[px1, px2,...], [py1, py2, ...], [py1, pz2, ...], [E1, E2, ...]]
+
+    target should be in the same format
+
+    :param *particles: parameter pack of array of particles to boost
+    :param target: array of particles to boost into. Should all be the same particle, i.e. a D meson
+    :param mass: mass of the target particles
+    :returns: tuple of arrays of boosted particles
+
+    """
+    # Number of target particles - i.e. number of boosts we need to perform
+    n = target.shape[1]
+
+    # Work out masses of target particles
+    masses = _masses(target[3], *target[0:3])
+
+    # Find gamma for each target particle
+    gammas = target[3] / masses
+    betas = np.sqrt(1 - 1 / gammas ** 2)
+
+    # Find direction for each target particle
+    directions = (
+        target[0:3] / np.sqrt(target[0] ** 2 + target[1] ** 2 + target[2] ** 2)
+    ).T
+
+    boosted_particles = []
+
+    # For each particle:
+    for particle in particles:
+        particle_energies = particle[3]
+        particle_3momenta = particle[0:3].T
+
+        # Init empty array for solutions
+        boosted_particle = np.zeros(target.shape)
+
+        # Work out energies
+        n_dot_p = (directions * particle_3momenta).sum(
+            1
+        )  # This is the dot product directions . 3-momenta
+        energies = gammas * (particle_energies - betas * n_dot_p)
+
+        # Work out momenta
+        momenta = (
+            particle_3momenta
+            + _multiply((gammas - 1), _multiply(n_dot_p, directions))
+            - _multiply(particle_energies * betas * gammas, directions)
+        )
+        boosted_particle[0:3] = momenta.T
+        boosted_particle[3] = energies
+
+        boosted_particles.append(boosted_particle)
+
+    return tuple(boosted_particles)
---
+++ test_boosts.py
@@ -0,0 +1,174 @@
+import numpy as np
+import boosts
+
+D_MASS_GEV = 1.86484
+K_MASS_GEV = 0.493677
+PI_MASS_GEV = 0.139570
+
+
+def test_no_boost():
+    # Stationary D
+    d_momentum = np.array([0.0, 0.0, 0.0, D_MASS_GEV])
+
+    gamma = boosts._gamma(D_MASS_GEV, d_momentum[3])
+    dirn = d_momentum[:3]
+
+    # vector to boost
+    target = np.array([1, 2, 3, 4])
+
+    # Check that it's unaffected
+    assert np.all(boosts.boost(target, gamma, dirn) == target)
+
+
+def test_x_boost():
+    """
+    Boost along the x axis
+
+    """
+    d_3momentum = [5.0, 0.0, 0.0]
+    d_energy = np.sqrt(np.linalg.norm(d_3momentum) ** 2 + D_MASS_GEV ** 2)
+    d_4momentum = np.array([*d_3momentum, d_energy])
+
+    k_3momentum = [0.0, 3.0, -4.0]
+    k_energy = np.sqrt(np.linalg.norm(k_3momentum) ** 2 + K_MASS_GEV ** 2)
+    k_4momentum = np.array([*k_3momentum, k_energy])
+
+    gamma = boosts._gamma(D_MASS_GEV, d_energy)
+    dirn = np.array([1.0, 0.0, 0.0])
+
+    expected = np.array([-13.47280853, 3.0, -4.0, 14.37916154])
+
+    assert np.allclose(expected, boosts.boost(k_4momentum, gamma, dirn), atol=0.01)
+
+
+def test_general_boost():
+    """
+    Boost along a not-nice direction
+
+    """
+    d_3momentum = np.array([1.0, 2.0, 3.0])
+    d_energy = np.sqrt(np.linalg.norm(d_3momentum) ** 2 + D_MASS_GEV ** 2)
+    d_4momentum = np.array([*d_3momentum, d_energy])
+
+    k_3momentum = [1.0, 3.0, -4.0]
+    k_energy = np.sqrt(np.linalg.norm(k_3momentum) ** 2 + K_MASS_GEV ** 2)
+    k_4momentum = np.array([*k_3momentum, k_energy])
+
+    gamma = boosts._gamma(D_MASS_GEV, d_energy)
+    dirn = boosts.direction(d_3momentum)
+
+    expected = np.array([-2.190947286, -3.381894571, -13.57284186, 14.16697618])
+
+    assert np.allclose(expected, boosts.boost(k_4momentum, gamma, dirn), atol=0.01)
+
+
+def test_masses():
+    energies = np.array([10.0, 15.0])
+    px = np.array([1.0, 2.0])
+    py = np.array([-2.0, 0.0])
+    pz = np.array([3.0, 4.0])
+
+    masses = np.array([np.sqrt(86), np.sqrt(205)])
+
+    assert np.allclose(boosts._masses(energies, px, py, pz), masses)
+
+
+def test_multiply():
+    assert np.allclose(
+        boosts._multiply(np.array([1, 2]), np.array([[1, 2, 3], [4, 5, 6]])),
+        np.array([[1, 2, 3], [8, 10, 12]]),
+    )
+
+
+def test_boosts():
+    """
+    Test numpy array of boosts
+
+    """
+    d = np.array(
+        [
+            [5.0, 1.0],
+            [0.0, 2.0],
+            [0.0, 3.0],
+            [
+                np.sqrt(25.0 + D_MASS_GEV ** 2),
+                np.sqrt(14.0 + D_MASS_GEV ** 2),
+            ],
+        ]
+    )
+    k = np.array(
+        [
+            [0.0, 1.0],
+            [3.0, 3.0],
+            [-4.0, -4.0],
+            [
+                np.sqrt(25.0 + K_MASS_GEV ** 2),
+                np.sqrt(26.0 + K_MASS_GEV ** 2),
+            ],
+        ]
+    )
+
+    expected = np.array(
+        [
+            [-13.47280853, -2.190947286],
+            [3.0, -3.381894571],
+            [-4.0, -13.57284186],
+            [14.37916154, 14.16697618],
+        ]
+    )
+    (calculated,) = boosts.boosts(k, target=d)
+
+    assert np.allclose(calculated, expected, atol=0.01)
+
+
+def test_boosts_multiple_particles():
+    d = np.array(
+        [
+            [5.0, 1.0],
+            [0.0, 2.0],
+            [0.0, 3.0],
+            [
+                np.sqrt(25.0 + D_MASS_GEV ** 2),
+                np.sqrt(14.0 + D_MASS_GEV ** 2),
+            ],
+        ]
+    )
+    k = np.array(
+        [
+            [0.0, 1.0],
+            [3.0, 3.0],
+            [-4.0, -4.0],
+            [
+                np.sqrt(25.0 + K_MASS_GEV ** 2),
+                np.sqrt(26.0 + K_MASS_GEV ** 2),
+            ],
+        ]
+    )
+
+    pi = np.array(
+        [
+            [0.0, 1.0],
+            [3.0, 3.0],
+            [-4.0, -4.0],
+            [
+                np.sqrt(25.0 + PI_MASS_GEV ** 2),
+                np.sqrt(26.0 + PI_MASS_GEV ** 2),
+            ],
+        ]
+    )
+
+    expected_k = np.array(
+        [
+            [-13.47280853, -2.190947286],
+            [3.0, -3.381894571],
+            [-4.0, -13.57284186],
+            [14.37916154, 14.16697618],
+        ]
+    )
+
+    # Can only be bothered to test the pion energy
+    expected_pi_energy = 14.11780251
+    boosted_k, boosted_pi = boosts.boosts(k, pi, target=d)
+
+    assert np.allclose(boosted_k, expected_k, atol=0.01)
+    assert np.allclose(boosted_pi[-1, 1], expected_pi_energy, atol=0.01)
