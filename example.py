b081970ee3eb5450fd6369c458ed1c54e81bceab
Richard Lane <richard.lane@bristol.ac.uk>
2021-09-10 12:38:17 +0100

    skel example

---
+++ example.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from lorentz_boost import boosts
+import pandas
+import numpy as np
+
+# Download some open data from CMS
+# Find x,y,z, energy of electrons
+# Find centre of mass 4-vectors, assuming they came from a J/psi
+# Find angle between electron 1 and z-axis before boost
+# Find angle between electron 1 and z-axis after boost
+
+# Plot before
+# Plot after
