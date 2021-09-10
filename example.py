e78a611b614680d78af0fd3cebf0be17437e94e1
Richard Lane <richard.lane@bristol.ac.uk>
2021-09-10 14:28:50 +0100

    plot angles

--- example.py
+++ example.py
@@ -1,13 +1,49 @@
 import matplotlib.pyplot as plt
-from lorentz_boost import boosts
-import pandas
+
+# from lorentz_boost import boosts
+import pandas as pd
 import numpy as np
 
+
+def angle(x1, y1, z1, x2, y2, z2):
+    r1 = np.column_stack((x1, y1, z1))
+    r2 = np.column_stack((x2, y2, z2))
+
+    norm1 = np.linalg.norm(r1, axis=1)
+    norm2 = np.linalg.norm(r2, axis=1)
+
+    dot = np.sum(r1 * r2, axis=1)
+    dot /= norm1 * norm2
+
+    return np.arccos(dot)
+
+
 # Download some open data from CMS
-# Find x,y,z, energy of electrons
+dataframe = pd.read_csv("https://opendata.cern.ch/record/5203/files/Jpsimumu.csv")
+
+# Find x,y,z, energy of muons
+px1 = dataframe["px1"].to_numpy()
+py1 = dataframe["py1"].to_numpy()
+pz1 = dataframe["pz1"].to_numpy()
+energy1 = dataframe["px1"].to_numpy()
+
+px2 = dataframe["px2"].to_numpy()
+py2 = dataframe["py2"].to_numpy()
+pz2 = dataframe["pz2"].to_numpy()
+energy2 = dataframe["px2"].to_numpy()
+
 # Find centre of mass 4-vectors, assuming they came from a J/psi
-# Find angle between electron 1 and z-axis before boost
-# Find angle between electron 1 and z-axis after boost
+jspi_mass_gev = 3096.900
+jpsi_px = px1 + px2
+jpsi_py = py1 + py2
+jpsi_pz = pz1 + pz2
+
+angles = angle(px1, py1, pz1, px2, py2, pz2)
+plt.hist(angles, bins=100)
+plt.show()
+
+# Find angle between muon 1 and z-axis before boost
+# Find angle between muon 1 and z-axis after boost
 
 # Plot before
 # Plot after
